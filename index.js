a370d3a09cfe65d5c04a142d1dffdc9d50b20bee
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-09 20:18:39 +0200

    mongoose dbConnection esatblished in index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const { dbConnection } = require("./config/config");
+
+app.use(express.json());
+
+dbConnection();
 
 app.use(PORT, () => {
   console.log(`Server running on port ${PORT}`);
