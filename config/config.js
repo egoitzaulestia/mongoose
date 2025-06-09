dd726db302064d6b6956c5f7b31035142c093d2d
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-09 20:16:21 +0200

    config.js with mongoose connection ready

--- config/config.js
+++ config/config.js
@@ -1,2 +1,16 @@
 const mongoose = require("mogoose");
 const { MONGO_URI } = require("./keys");
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error while initializeng the database");
+  }
+};
+
+module.exports = {
+  dbConnection,
+};
