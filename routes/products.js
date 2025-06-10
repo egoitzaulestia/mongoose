ce322e6b6501239e549e6e3cc8d33bc70a497bf6
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-10 19:03:04 +0200

    getProductsByName route ready and working

--- routes/products.js
+++ routes/products.js
@@ -5,5 +5,6 @@ const ProductController = require("../controllers/ProductController");
 router.post("/", ProductController.create);
 router.get("/", ProductController.getAll);
 router.get("/id/:id", ProductController.getById);
+router.get("/name/:name", ProductController.getProductsByName);
 
 module.exports = router;
