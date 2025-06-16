93e6fea1c80aa1885d9a408ec2bef46845d1ab92
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-16 12:55:04 +0200

    product authentication routes middleware

--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const ProductController = require("../controllers/ProductController");
+const { authentication } = require("../middlewares/authentication");
 
-router.post("/", ProductController.create);
+router.post("/", authentication, ProductController.create);
 router.get("/", ProductController.getAll);
 router.get("/id/:id", ProductController.getById);
 router.get("/name/:name", ProductController.getProductsByName);
 router.get("/nameIndex/:nameIndex", ProductController.getProductsByNameIndex);
-router.put("/id/:id", ProductController.update);
-router.delete("/id/:id", ProductController.delete);
+router.put("/id/:id", authentication, ProductController.update);
+router.delete("/id/:id", authentication, ProductController.delete);
 
 module.exports = router;
