b2a7da4ea8a7cc35aeed3fec9710f3769c473222
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-16 16:35:09 +0200

    isAdmin ready and working

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -28,11 +28,12 @@ const authentication = async (req, res, next) => {
 };
 
 const isAdmin = async (req, res, next) => {
-  const admins = ["admins", "superadmin"];
+  const admins = ["admin", "superadmin"];
 
   if (!admins.includes(req.user.role)) {
     return res.status(403).send({ message: "You do not have permission" });
   }
+  next();
 };
 
 module.exports = { authentication, isAdmin };
