56003991eb80fba215ba255a21707e68825c70cc
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-16 19:27:04 +0200

    product model updated by adding reviewes comment

--- models/Product.js
+++ models/Product.js
@@ -1,11 +1,19 @@
 const mongoose = require("mongoose");
+const ObjectId = mongoose.SchemaTypes.ObjectId;
 
 const ProductSchema = new mongoose.Schema(
   {
     name: String,
     price: Number,
+    reviews: [
+      {
+        userId: { type: ObjectId, ref: "User" },
+        comment: String,
+      },
+      { timestamps: true }, // timestamps of the comment
+    ],
   },
-  { timestamps: true }
+  { timestamps: true } // timestamps of the product
 );
 
 ProductSchema.index({
@@ -13,4 +21,5 @@ ProductSchema.index({
 });
 
 const Product = mongoose.model("Product", ProductSchema);
+
 module.exports = Product;
