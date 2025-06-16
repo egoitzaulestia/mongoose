8d67a98d471ca7214ed920fd5e83d748b2450763
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-16 16:43:20 +0200

    isAuthor instaured in order update route

--- routes/orders.js
+++ routes/orders.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const OrderController = require("../controllers/OrderController");
-const { authentication } = require("../middlewares/authentication");
+const { authentication, isAuthor } = require("../middlewares/authentication");
 
 router.post("/", authentication, OrderController.create);
-router.put("/:_id", authentication, OrderController.update);
+router.put("/:_id", authentication, isAuthor, OrderController.update);
 
 module.exports = router;
