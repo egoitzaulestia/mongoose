df7cbdddd4691b8c21eb90289d6b813866876b8a
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-16 11:12:03 +0200

    UserController file and register method created

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,1 +1,15 @@
 const User = require("../models/User");
+
+const UserController = {
+  async register(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.status(201).send({ message: "User registered successfully", user });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: "Error", error });
+    }
+  },
+};
+
+module.exports = UserController;
