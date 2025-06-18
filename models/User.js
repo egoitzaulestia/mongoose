40429b31606d896c970e48a9aee260d8adf5af11
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-18 18:43:49 +0200

    user schema model changed

--- models/User.js
+++ models/User.js
@@ -3,12 +3,27 @@ const ObjectId = mongoose.SchemaTypes.ObjectId;
 
 const UserSchema = new mongoose.Schema(
   {
-    name: String,
-    email: String,
-    password: String,
-    age: Number,
-    tokens: [],
+    name: {
+      type: String,
+      required: [true, "Por favor rellena tu nombre"],
+    },
+    email: {
+      type: String,
+      match: [/.+\@.+\..+/, "Este correo no es válido"],
+      unique: true,
+      required: [true, "Por favor rellena tu correo"],
+    },
+    password: {
+      type: String,
+      required: [true, "Por favor rellena tu contraseña"],
+    },
+    age: {
+      type: Number,
+      required: [true, "Por favor rellena tu edad"],
+    },
+
     role: String,
+    tokens: [],
     orderIds: [{ type: ObjectId, ref: "Order" }],
   },
   { timestamps: true }
